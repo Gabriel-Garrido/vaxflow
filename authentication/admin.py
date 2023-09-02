2c0cb3c3d26735e6a2946b0f1ca22388e42eb416
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-01 21:11:42 -0400

    orden en campos de crear usuario en django admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,21 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser  # Importa el modelo de usuario personalizado
 from inventario.models import Vacunatorio
 
+# Define una clase CustomUserAdmin que personaliza la vista de administración de usuarios
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'rut', 'vacunatorio', 'name', 'last_name', 'phone')
-    list_filter = ('groups', 'vacunatorio')
+    # Muestra estas columnas en la lista de usuarios en el panel de administración
+    list_display = ('username', 'name', 'last_name', 'vacunatorio', 'is_staff', 'is_superuser' )
+    
+    # Agrega filtros para las columnas de grupos y vacunatorio en el panel de administración
+    list_filter = ('groups', 'vacunatorio', 'is_staff', 'is_superuser')
+    
+    # Define secciones y campos en la vista de detalle del usuario en el panel de administración
     fieldsets = (
         ('Personal Info', {'fields': ('username', 'password', 'email', 'rut', 'vacunatorio', 'name', 'last_name', 'phone')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important Dates', {'fields': ('last_login',)}),
     )
+    
+    # Define los campos que se mostrarán al agregar un nuevo usuario en el panel de administración
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'rut', 'vacunatorio', 'name', 'last_name', 'phone', 'groups', 'password1', 'password2'),
+            'fields': ('username','rut', 'name', 'last_name','email', 'phone', 'vacunatorio', 'password1', 'password2', 'groups'),
         }),
     )
 
+# Registra el modelo CustomUser con la configuración personalizada en el panel de administración
 admin.site.register(CustomUser, CustomUserAdmin)
