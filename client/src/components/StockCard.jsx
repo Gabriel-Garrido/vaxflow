b2ccb37a1f4da0c8120fad8aa988c68748b3429e
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-02 23:13:27 -0400

    se agregan botones de traspaso, eliminacion y administradas del dia en stock

--- client/src/components/StockCard.jsx
+++ client/src/components/StockCard.jsx
@@ -5,29 +5,62 @@ import { Traspaso } from './Traspaso';
 export function StockCard({ stock }) {
   return (
     <div key={stock.id} className="card text-white mb-3">
-      <div className="card-body text-start">
-        {stock.fecha_descongelacion && stock.hora_descongelacion && (
+      <div className="card-body text-start row">
+        <div className='col-6'>
+          {stock.fecha_descongelacion && stock.hora_descongelacion && (
+            <p className="mb-0">
+              <strong>Descongeladas el:</strong> <br /> {' '}
+              {format(new Date(stock.fecha_descongelacion), 'dd MMM yyyy', { locale: es })}{' '}
+              - {format(new Date(`1970-01-01T${stock.hora_descongelacion}`), 'hh:mm')}
+            </p>
+            
+          )}
+          <br />
+          {stock.fecha_caducidad_descongelacion && (
+            <p className="mb-0">
+              <strong>Caduca el:</strong> <br /> {' '}
+              {format(new Date(stock.fecha_caducidad_descongelacion), 'dd MMM yyyy', { locale: es })}{' '}
+              - {format(new Date(`1970-01-01T${stock.hora_descongelacion}`), 'hh:mm')}
+            </p>
+          )}
+          <hr />
           <p className="mb-0">
-            <strong>Descongeladas el:</strong>{' '}
-            {format(new Date(stock.fecha_descongelacion), 'eee dd MMM yyyy', { locale: es })}{' '}
-            a las {format(new Date(`1970-01-01T${stock.hora_descongelacion}`), 'hh:mm')}
+            <strong>Caducidad fabricante:</strong>{' '}
+            {format(new Date(stock.caducidad_fabricante), 'eee dd MMM yyyy', { locale: es })}
           </p>
-        )}
-        {stock.fecha_caducidad_descongelacion && (
-          <p className="mb-0">
-            <strong>Caduca el:</strong>{' '}
-            {format(new Date(stock.fecha_caducidad_descongelacion), 'eee dd MMM yyyy', { locale: es })}
-          </p>
-        )}
-        <p className="mb-0">
-          <strong>Caducidad fabricante:</strong>{' '}
-          {format(new Date(stock.caducidad_fabricante), 'eee dd MMM yyyy', { locale: es })}
-        </p>
+        </div>
 
-        {/* Botón modal */}
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#offcanvas${stock.id}`}>
-          Link with href
+        <div className="container col-6">
+  <div className="row text-center">
+    <div className="col-12 mb-2">
+      <div className="btn-group d-flex" role="group">
+        {/* Botón modal traspaso */}
+        <button type="button" className="btn btn-primary flex-fill" data-bs-toggle="modal" data-bs-target={`#offcanvas${stock.id}`}>
+          Traspaso
         </button>
+      </div>
+    </div>
+    <div className="col-12 mb-2">
+      <div className="btn-group d-flex" role="group">
+        {/* Botón modal registro de administradas */}
+        <button type="button" className="btn btn-primary flex-fill" data-bs-toggle="modal" data-bs-target={`#offcanvas${stock.id}`}>
+          Registrar administradas del día
+        </button>
+      </div>
+    </div>
+    <div className="col-12 mt-4">
+      <div className="btn-group d-flex" role="group">
+        {/* Botón modal eliminación */}
+        <button type="button" className="btn btn-danger flex-fill" data-bs-toggle="modal" data-bs-target={`#offcanvas${stock.id}`}>
+          Eliminar
+        </button>
+      </div>
+    </div>
+    
+  </div>
+</div>
+
+
 
         {/* Modal */}
         <div className="modal fade" tabIndex="-1" id={`offcanvas${stock.id}`} aria-labelledby={`offcanvas${stock.id}Label`} aria-hidden="true">
