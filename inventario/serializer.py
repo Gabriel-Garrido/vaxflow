c70dae8a4326f99181e0d693d959164bc8581681
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-06 00:53:11 -0300

    correccion en serializers de traspaso

--- inventario/serializer.py
+++ inventario/serializer.py
@@ -30,8 +30,8 @@ class TraspasoVacunaSerializer(serializers.ModelSerializer):
     vacuna_traspaso_nombre = serializers.CharField(source='vacuna_traspaso.tipo_vacuna.nombre', read_only=True)
     vacuna_traspaso_lote = serializers.CharField(source='vacuna_traspaso.tipo_vacuna.lote', read_only=True)
     vacuna_traspaso_caducidad = serializers.DateField(source='vacuna_traspaso.tipo_vacuna.fecha_caducidad_fabricante', read_only=True)
-    vacunatorio_origen = serializers.CharField(source='vacuna_traspaso.vacunatorio.nombre', read_only=True)
-    vacunatorio_destino = serializers.CharField(source='vacunatorio_destino.nombre', read_only=True)
+    vacunatorio_origen_nombre = serializers.CharField(source='vacuna_traspaso.vacunatorio.nombre', read_only=True)
+    vacunatorio_destino_nombre = serializers.CharField(source='vacunatorio_destino.nombre', read_only=True)
     responsable_entrega_nombre = serializers.CharField(source='responsable_entrega.name', read_only=True)
     responsable_recepcion_nombre = serializers.CharField(source='responsable_recepcion.name', read_only=True)
 
