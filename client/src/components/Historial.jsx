a1d327f78cf0163097bb560ab63df725828dd1a5
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-17 02:27:05 -0300

    mejoras en bootstrap de filtros de historial

--- client/src/components/Historial.jsx
+++ client/src/components/Historial.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { getAllTraspasos } from '../api/inventario';
 import { useAuth } from '../AuthContext';
 
-export function Historial({userDetails}) {
+export function Historial({ userDetails }) {
   const [traspasos, setTraspasos] = useState([]);
   const [tipoTraspaso, setTipoTraspaso] = useState('todos');
   const [fechaInicio, setFechaInicio] = useState('');
   const [fechaFin, setFechaFin] = useState('');
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     setLoading(true);
+
     async function fetchTraspasos() {
       try {
         const response = await getAllTraspasos();
@@ -61,90 +61,88 @@ export function Historial({userDetails}) {
       </div>
     );
   } else {
-    
     return (
-    <div className='card text-bg-dark' style={{ maxHeight: '75vh', overflowY: 'auto' }}>
-      <h2 className='card-title fs-4 mt-2 text-success text-center'>Historial de Traspasos</h2>
-      <div className="card-body text-center fs-3">
-        <label htmlFor="tipoTraspaso" className="form-label-sm">
-          Tipo de Traspaso:
-        </label>
-        <select
-          id="tipoTraspaso"
-          className="form-select-sm"
-          value={tipoTraspaso}
-          onChange={(e) => setTipoTraspaso(e.target.value)}
-        >
-          <option value="todos">Todos</option>
-          <option value="enviados">Enviados</option>
-          <option value="recibidos">Recibidos</option>
-        </select>
-      </div>
-      <div className="mb-1 text-center">
-        <label htmlFor="fechaInicio" className="form-label-sm ">
-          Fecha de Inicio:
-        </label>
-        <input
-          type="date"
-          id="fechaInicio"
-          className="form-control-sm"
-          value={fechaInicio}
-          onChange={(e) => setFechaInicio(e.target.value)}
-        />
-      </div>
-      <div className="mb-2 text-center">
-        <label htmlFor="fechaFin" className="form-label-sm">
-          Fecha de Fin:
-        </label>
-        <input
-          type="date"
-          id="fechaFin"
-          className="form-control-sm"
-          value={fechaFin}
-          onChange={(e) => setFechaFin(e.target.value)}
-        />
-      </div>
-      <table className="table table-dark text-center">
-        <thead>
-          <tr>
-            <th><i className="fa-solid fa-calendar-check text-success"></i></th>
-            <th><i className="fa-solid fa-syringe text-success"></i></th>
-            <th><i className="fa-solid fa-right-left text-success"></i></th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTraspasosByFecha.reverse().map((traspaso) => (
-            <tr key={traspaso.id} className='custom-tabe-style text-center'>
-              <td >
-                {new Date(traspaso.fecha_traspaso).toLocaleDateString()}
-              </td>
-              {traspaso.responsable_entrega === userDetails.id ? (<>
-                <td>
-                  <strong>
-                    {traspaso.cantidad_traspasada} dosis de <br /> {traspaso.vacuna_traspaso_nombre}
-                  </strong>{' '}
-                </td>
-                <td>
-                <i className="fa-regular fa-paper-plane fs-6"></i> <br /><strong>{traspaso.vacunatorio_destino_nombre}</strong>
-                </td>
-                </>
-              ) : (
-                <>
-                <td>
-                  <strong>
-                    {traspaso.cantidad_traspasada} dosis de <br /> {traspaso.vacuna_traspaso_nombre}
-                  </strong>{' '}
-                </td>
-                <td>
-                <i className="fa-regular fa-circle-down fs-6"></i><br /> <strong>{traspaso.vacunatorio_origen_nombre}</strong>
-                </td>
-                </>
-              )}
+      <div className='card text-bg-dark' style={{ maxHeight: '75vh', overflowY: 'auto' }}>
+        <h2 className='card-title fs-4 mt-2 text-success text-center'>Historial de Traspasos</h2>
+        <div className="card-body">
+          <div className="row justify-content-center mb-1">
+            <div className="col-md-4 col-sm-12 text-center">
+              <label htmlFor="tipoTraspaso" className="form-label-sm">Traspaso:</label>
+              <select
+                id="tipoTraspaso"
+                className="form-select form-select-sm custom-tabe-style"
+                value={tipoTraspaso}
+                onChange={(e) => setTipoTraspaso(e.target.value)}
+              >
+                <option value="todos">Todos</option>
+                <option value="enviados">Enviados</option>
+                <option value="recibidos">Recibidos</option>
+              </select>
+            </div>
+            <div className="col-md-4 col-sm-6 text-center">
+              <label htmlFor="fechaInicio" className="form-label-sm">Desde:</label>
+              <input
+                type="date"
+                id="fechaInicio"
+                className="form-control form-control-sm custom-tabe-style "
+                value={fechaInicio}
+                onChange={(e) => setFechaInicio(e.target.value)}
+              />
+            </div>
+            <div className="col-md-4 col-sm-6 text-center">
+              <label htmlFor="fechaFin" className="form-label-sm">Hasta:</label>
+              <input
+                type="date"
+                id="fechaFin"
+                className="form-control form-control-sm custom-tabe-style"
+                value={fechaFin}
+                onChange={(e) => setFechaFin(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <table className="table table-dark text-center">
+          <thead>
+            <tr>
+              <th><i className="fa-solid fa-calendar-check text-success"></i></th>
+              <th><i className="fa-solid fa-syringe text-success"></i></th>
+              <th><i className="fa-solid fa-right-left text-success"></i></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+          </thead>
+          <tbody>
+            {filteredTraspasosByFecha.reverse().map((traspaso) => (
+              <tr key={traspaso.id} className='custom-tabe-style text-center'>
+                <td>
+                  {new Date(traspaso.fecha_traspaso).toLocaleDateString()}
+                </td>
+                {traspaso.responsable_entrega === userDetails.id ? (
+                  <>
+                    <td>
+                      <strong>
+                        {traspaso.cantidad_traspasada} dosis de <br /> {traspaso.vacuna_traspaso_nombre}
+                      </strong>{' '}
+                    </td>
+                    <td>
+                      <i className="fa-regular fa-paper-plane fs-6"></i> <br /><strong>{traspaso.vacunatorio_destino_nombre}</strong>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>
+                      <strong>
+                        {traspaso.cantidad_traspasada} dosis de <br /> {traspaso.vacuna_traspaso_nombre}
+                      </strong>{' '}
+                    </td>
+                    <td>
+                      <i className="fa-regular fa-circle-down fs-6"></i><br /> <strong>{traspaso.vacunatorio_origen_nombre}</strong>
+                    </td>
+                  </>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
