d66f2d8eb7f808f7a59096d26cf81ad0ea997ade
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-21 22:04:10 -0300

    agrego container a home y row a y row a home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Reportes } from '../components/home/Reportes'
 import { Stock } from '../components/home/Stock'
 
 export function Home() {
-    const { isAuthenticated, setIsAuthenticated, user, userDetails, fetchStock, stock } = useAuth();
+    const { userDetails } = useAuth();
 
 
     if (!userDetails) {
@@ -20,7 +20,7 @@ export function Home() {
       } else {
     return (
         <div className=' mt-2'>
-        <div className='row'>
+        <div className='row container'>
             <div className='col-lg-4 d-none d-xl-block'>
                 <div className='container col-12'>
                     <Historial userDetails={userDetails} />
@@ -39,7 +39,8 @@ export function Home() {
         </div>
     
         {/* Small screen */}
-        <nav className='d-flex mb-0 d-xl-none '>
+        <div className='container row'>
+        <nav className='d-block mb-0 d-xl-none '>
             <div className="nav nav-tabs align-content-top col-12 d-xl-none text-center" id="nav-tab" role="tablist">
                 <button className="nav-link col-4" id="nav-historial-tab" data-bs-toggle="tab" data-bs-target="#nav-historial" type="button" role="tab" aria-controls="nav-historial" aria-selected="true"><i className="fa-solid fa-book fs-3"></i></button>
                 <button className="nav-link active col-4" id="nav-stock-tab" data-bs-toggle="tab" data-bs-target="#nav-stock" type="button" role="tab" aria-controls="nav-stock" aria-selected="false"><i className="fa-solid fa-syringe fs-3"></i></button>
@@ -56,6 +57,7 @@ export function Home() {
             <div className="tab-pane fade" id="nav-reporte" role="tabpanel" aria-labelledby="nav-reporte-tab" tabIndex="0">
                 <Reportes userDetails={userDetails} />
             </div>
+        </div>
         </div>
         {/* //Small screen */}
     </div>
