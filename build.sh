741ddc7dc6994ac12504ebace14d1391211dd787
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-10-01 01:15:10 -0300

    agrego creacion de superuser segun variable de entorno

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear un superusuario de Django
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'gabrielgarrido89@gmail.com', 'gabriel43')" | python manage.py shell
