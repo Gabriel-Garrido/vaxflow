c92cbebafa816977f429ec71bbfca83c8bff6852
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-09-30 21:27:04 -0300

    comento requierement en build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+#render.com ejecunta esto automaticamente, por eso lo dejo comentado
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
