c24e56c4dc1c5012886bad77124ad47b5a4bdd46
Gabriel Garrido <gabrielgarrido89@gmail.com>
2023-11-02 12:54:05 -0300

    cambio database en settings.py

--- vaxflow_api/settings.py
+++ vaxflow_api/settings.py
@@ -90,9 +90,15 @@ WSGI_APPLICATION = 'vaxflow_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
